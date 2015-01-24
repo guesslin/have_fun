71ee0ce2432ac8df3fd6b737f6e923bc237c9ebc
Ian.Yang <executor@executor-macbookpro3.roam.corp.google.com>
2015-01-24 16:35:49 +0800

    New file midpoint.go with newly created funciton LookingForMidpoint for looking midpoint between two positions.

---
+++ services/midpoint.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func LookingForMidpoint( position1 GPS, position2 GPS ) GPS {
+	var Position GPS
+	Position.Lat = ( position1.Lat + position2.Lat ) / 2
+	Position.Lon = ( position1.Lon + position2.Lon ) / 2
+	return Position
+}
