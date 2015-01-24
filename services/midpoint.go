cab68e2e7318b3ae3b7dbceb71228de557afebfc
Ian Yang <executor.tw@gmail.com>
2015-01-24 18:06:39 +0800

    Fix:midpoint.go format fix

--- services/midpoint.go
+++ services/midpoint.go
@@ -1,8 +1,8 @@
 package service
 
-func LookingForMidpoint( position1 GPS, position2 GPS ) GPS {
+func LookingForMidpoint(position1 GPS, position2 GPS) GPS {
 	var Position GPS
-	Position.Lat = ( position1.Lat + position2.Lat ) / 2
-	Position.Lon = ( position1.Lon + position2.Lon ) / 2
+	Position.Lat = (position1.Lat + position2.Lat) / 2
+	Position.Lon = (position1.Lon + position2.Lon) / 2
 	return Position
 }
